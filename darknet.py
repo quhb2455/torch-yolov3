a298d92ae0e47ca328c5fbf22d0f0deb8485c15a
quhb2455 <quhb2455@gmail.com>
2021-03-12 00:02:25 +0900

    -

--- darknet.py
+++ darknet.py
@@ -5,6 +5,11 @@ from torch.autograd import Variable
 
 import numpy as np
 
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
 def parse_cfg(file) :
 
     cfg_file = open(file, 'r')
@@ -45,12 +50,81 @@ def create_modules(blocks):
     prev_filters = 3
     output_filters = []
 
-    
+    for index, x in enumerate(blocks[1:]) :
+        module = nn.Sequential()
+
+        # convolution
+        if(x['type'] == "convolutional") :
+            activation = x['activation']
+
+            try :
+                batch_normalize = int(x["batch_normalize"])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+
+            filters = int(x["filters"])
+            paddding = int(x["pad"])
+            kernel_size = int(x["size"])
+            stride = int(x["stride"])
+
+            if paddding :
+                pad = (kernel_size - 1) // 2
+            else :
+                pad = 0
+
+            # convolution layer 추가
+            conv = nn.Conv2d(prev_filters, filters, kernel_size=kernel_size, stride=stride, paddding=pad, bias=bias)
+
+            # module name , module
+            module.add_module("conv_{0}".format(index), conv)
+
+            # batch normalization 추가
+            if batch_normalize :
+                bn = nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index), bn)
+
+            if activation == "leaky" :
+                act_fn = nn.LeakyReLU(0.1, inplace=True) # inplace => input으로 들어온 값 자체를 수정.
+                module.add_module("activateion_fn_{0}".format(index), act_fn)
+
+        # upsample
+        elif(x['type'] == 'upsample'):
+
+            stride = int(x['stride'])
+            upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+            module.add_module("upsample_{0}".format(index), upsample)
+
+        elif(x['type'] == 'route') :
+            x["layer"] = x['layer'].split(',')
+            start_route = int(x['layer'][0])
+
+            # layer에 값이 1개가 있을 수도 있음.
+            try :
+                end_route = int(x['layer'][1])
+            except :
+                end = 0
+
+            if start_route > 0 :
+                start_route = start_route - index
+            if end_route > 0 :
+                end_route = end_route -index
+
+            route = EmptyLayer()
+            module.add_module("route_{0}".format(index), route)
+
+            if end_route < 0 :
+                filters = output_filters[index + start_route] + output_filters[index + end_route]
+            else :
+                filters = output_filters[index + start_route]
 
 
+        elif(x['type'] == 'shortcut'):
+            shortcut = EmptyLayer()
+            module.add_module("shortcut_{0}".format(index), shortcut)
 
-
-
+ # create_modules(parse_cfg("./cfg/yolov3.cfg"))
 
 
 
